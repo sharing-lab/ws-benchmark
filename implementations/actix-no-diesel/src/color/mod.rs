20c91928621637c7c8469b157531001d150fd8ef
Fatih <fatih.bdg@gmail.com>
2020-02-11 13:35:55 +0700

    use r2d2_postgres = "0.16"

--- implementations/actix-no-diesel/src/color/mod.rs
+++ implementations/actix-no-diesel/src/color/mod.rs
@@ -3,18 +3,19 @@ use actix_web::{web, HttpResponse, Error};
 use r2d2_postgres::PostgresConnectionManager;
 use r2d2::Pool;
 use futures::Future;
+use postgres::{NoTls, Client};
 
 pub fn get_list(
-  db: web::Data<Pool<PostgresConnectionManager>>,
+    db: web::Data<Pool<PostgresConnectionManager<NoTls>>>,
 ) -> impl Future<Item = HttpResponse, Error = Error> {
 
   web::block(move || {
 
-    let conn = db.get().unwrap();
+    let mut conn = db.get().unwrap();
 
     let mut vec:Vec<common::Color> = Vec::new();
 
-    for row in &conn.query("SELECT id, code, name FROM color", &[]).unwrap() {
+    for row in conn.query("SELECT id, code, name FROM color", &[]).unwrap() {
       let color = common::Color {
         id: row.get(0),
         code: row.get(1),
--- implementations/actix-no-diesel/src/main.rs
+++ implementations/actix-no-diesel/src/main.rs
@@ -1,22 +1,23 @@
 mod color;
 extern crate r2d2;
 use actix_web::{web, App, HttpServer};
-use r2d2_postgres::{r2d2::Pool, PostgresConnectionManager, TlsMode};
+use postgres::{NoTls, Client};
+use r2d2_postgres::{r2d2::Pool, PostgresConnectionManager};
 // Handler functions here
  
 fn main() {
   std::env::set_var("RUST_LOG", "actix_web=info");
   env_logger::init();
 
-  let manager = PostgresConnectionManager::new(
-    "postgres://postgres@localhost/test_db",
-    TlsMode::None,
-    )
-    .unwrap();
-  let pool = Pool::builder().build(manager).unwrap();
+    let manager = PostgresConnectionManager::new(
+        "host=127.0.0.1 user=postgres dbname=test_db".parse().unwrap(),
+        NoTls);
+    //let postgres_pool = Pool::builder().build(manager).unwrap();
+    let pool = r2d2::Pool::new(manager).unwrap();
 
   HttpServer::new(move || {
     App::new()
+      //.data(postgres_pool.clone())
       .data(pool.clone())
       // enable logger
       .wrap(actix_web::middleware::Logger::default())
