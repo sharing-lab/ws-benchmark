11bdfa0f8c6e9e6ad6a3f10fc2e0070ac8e2244e
Fatih <fatih.bdg@gmail.com>
2020-02-11 13:46:12 +0700

    some comments added with short explanation from reference doc

--- implementations/actix-no-diesel/src/color/mod.rs
+++ implementations/actix-no-diesel/src/color/mod.rs
@@ -3,7 +3,7 @@ use actix_web::{web, HttpResponse, Error};
 use r2d2_postgres::PostgresConnectionManager;
 use r2d2::Pool;
 use futures::Future;
-use postgres::{NoTls, Client};
+use postgres::NoTls;
 
 pub fn get_list(
     db: web::Data<Pool<PostgresConnectionManager<NoTls>>>,
@@ -11,6 +11,10 @@ pub fn get_list(
 
   web::block(move || {
 
+    // why mut conn ?, answer : the reference documentation is here :
+    // -> https://docs.rs/r2d2_postgres/0.16.0/r2d2_postgres/struct.PostgresConnectionManager.html
+    // -> ...
+    // ->    let mut client = pool.get().unwrap(); 
     let mut conn = db.get().unwrap();
 
     let mut vec:Vec<common::Color> = Vec::new();
--- implementations/actix-no-diesel/src/main.rs
+++ implementations/actix-no-diesel/src/main.rs
@@ -1,23 +1,23 @@
 mod color;
 extern crate r2d2;
 use actix_web::{web, App, HttpServer};
-use postgres::{NoTls, Client};
-use r2d2_postgres::{r2d2::Pool, PostgresConnectionManager};
+use postgres::NoTls;
+use r2d2_postgres::PostgresConnectionManager;
 // Handler functions here
  
 fn main() {
   std::env::set_var("RUST_LOG", "actix_web=info");
   env_logger::init();
 
-    let manager = PostgresConnectionManager::new(
-        "host=127.0.0.1 user=postgres dbname=test_db".parse().unwrap(),
-        NoTls);
-    //let postgres_pool = Pool::builder().build(manager).unwrap();
-    let pool = r2d2::Pool::new(manager).unwrap();
+  // example as reference for these two statements below :
+  // -> https://docs.rs/r2d2_postgres/0.16.0/r2d2_postgres/struct.PostgresConnectionManager.html
+  let manager = PostgresConnectionManager::new(
+    "host=127.0.0.1 user=postgres dbname=test_db".parse().unwrap(),
+    NoTls);
+  let pool = r2d2::Pool::new(manager).unwrap();
 
   HttpServer::new(move || {
     App::new()
-      //.data(postgres_pool.clone())
       .data(pool.clone())
       // enable logger
       .wrap(actix_web::middleware::Logger::default())
