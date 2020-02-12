1d8ab890831d835ceeaed72169e71fe7e13fe7b8
Fatih <fatih.bdg@gmail.com>
2020-02-12 09:15:11 +0700

    logging enabled

--- implementations/actix-sync/src/color/mod.rs
+++ implementations/actix-sync/src/color/mod.rs
@@ -3,24 +3,12 @@ extern crate postgres;
 use postgres::{Client, NoTls};
 use actix_web::{Responder, web};
 
-static pg_connection_string: &str = "host=127.0.0.1 user=postgres dbname=test_db";
-
-// pub fn get_kapal_all(vec: &mut Vec<common::Kapal>) {
-//   let conn = Connection::connect(pg_connection_string, TlsMode::None) .unwrap();
-//   for row in &conn.query("SELECT id, kode FROM kapal", &[]).unwrap() {
-//     let kapal = common::Kapal {
-//       id: row.get(0),
-//       kode: row.get(1)
-//     };
-//     vec.push(kapal);
-//   }
-// }
+static PG_CONNECTION_STRING: &str = "host=127.0.0.1 user=postgres dbname=test_db";
 
 pub fn get_list() -> impl Responder {
   let mut vec:Vec<common::Color> = Vec::new();
 
-//  kapal::get_kapal_all(&mut vec);
-  let mut conn = Client::connect(pg_connection_string, NoTls) .unwrap();
+  let mut conn = Client::connect(PG_CONNECTION_STRING, NoTls) .unwrap();
   for row in conn.query("SELECT id, code, name FROM color", &[]).unwrap() {
     let color = common::Color {
       id: row.get(0),
--- implementations/actix-sync/src/main.rs
+++ implementations/actix-sync/src/main.rs
@@ -1,11 +1,14 @@
 mod color;
-use actix_web::{web, App, HttpServer, Responder};
+use actix_web::{web, App, HttpServer};
  
-// Handler functions here
- 
+
 fn main() {
+  std::env::set_var("RUST_LOG", "actix_web=info");
+  env_logger::init();
+
   HttpServer::new(|| {
     App::new()
+      .wrap(actix_web::middleware::Logger::default())
       .service(web::resource("/color")
         .route(web::get().to(color::get_list)) )
   })
