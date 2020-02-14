c09f51d47bb9c761d2aabfb7456bfaf01de6d4eb
Fatih <fatih.bdg@gmail.com>
2020-02-14 22:23:59 +0700

    max idle pool size = 100

--- implementations/spider-g/src/controllers/application.cr
+++ implementations/spider-g/src/controllers/application.cr
@@ -1,7 +1,7 @@
 require "pg"
 
 APPDB = DB.open "postgres://postgres@127.0.0.1/test_db?" +
-	"initial_pool_size=10&max_idle_pool_size=40"
+	"initial_pool_size=10&max_idle_pool_size=100"
 
 abstract class Application < ActionController::Base
 
