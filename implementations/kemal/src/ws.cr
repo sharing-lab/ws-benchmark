4a73ce5d32fdef67bf9337be93939fa067dcd3d9
Fatih <fatih.bdg@gmail.com>
2020-02-15 18:13:16 +0700

    max 100 pg connections

--- implementations/kemal/src/ws.cr
+++ implementations/kemal/src/ws.cr
@@ -3,7 +3,7 @@ require "pg"
 require "json"
 
 APPDB = DB.open "postgres://postgres@127.0.0.1/test_db?" +
-  "initial_pool_size=10&max_idle_pool_size=10"
+  "initial_pool_size=10&max_idle_pool_size=100"
 def db
   return APPDB
 end
