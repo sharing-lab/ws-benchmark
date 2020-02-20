32cf71c49c3d01e23aec2f6c0005ca8d17c985f5
Fatih <fatih.bdg@gmail.com>
2020-02-20 09:53:18 +0700

    fasthttp router : up & running

--- implementations/fasthttp-router/main.go
+++ implementations/fasthttp-router/main.go
@@ -18,14 +18,17 @@ var (
 func doJSONWrite(ctx *fasthttp.RequestCtx, code int, obj interface{}) {
 	ctx.Response.Header.SetCanonical(strContentType, strApplicationJSON)
 	ctx.Response.SetStatusCode(code)
-	start := time.Now()
 	if err := json.NewEncoder(ctx).Encode(obj); err != nil {
 		ctx.Error(err.Error(), fasthttp.StatusInternalServerError)
 	}
 }
 
+var (
+	db *sql.DB
+)
+
 // Index is the index handler
-func Index(ctx *fasthttp.RequestCtx, db *sql.DB) {
+func Index(ctx *fasthttp.RequestCtx) {
 	//fmt.Fprint(ctx, "Welcome!\n")
 	products, err := getProducts(db)
 	if err != nil {
@@ -36,13 +39,14 @@ func Index(ctx *fasthttp.RequestCtx, db *sql.DB) {
 }
 
 func main() {
+	var err error
 	db, err = sql.Open("postgres", "user=postgres dbname=test_db host=127.0.0.1 sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	r := router.New()
-	r.GET("/", Index(db))
+	r.GET("/", Index)
 
 	log.Fatal(fasthttp.ListenAndServe(":55518", r.Handler))
 }
