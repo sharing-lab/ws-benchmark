573d49250cbbc066b1223846bd6e0d4f06912dda
Fatih <fatih.bdg@gmail.com>
2020-02-22 22:32:06 +0700

    max-idle-pool size : 100

--- implementations/grip/src/ws.cr
+++ implementations/grip/src/ws.cr
@@ -2,7 +2,7 @@ require "grip"
 require "pg"
 
 APPDB = DB.open "postgres://postgres@127.0.0.1/test_db?" +
-  "initial_pool_size=10&max_idle_pool_size=10"
+  "initial_pool_size=10&max_idle_pool_size=100"
 def db
   return APPDB
 end
